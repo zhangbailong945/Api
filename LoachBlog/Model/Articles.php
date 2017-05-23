c6389cef96d6deea1be5b08235faf263c5105ffa
zhangbailong945 <1207549344@qq.com>
2017-05-23 22:57:43 +0800

    update getAllArticles api

--- LoachBlog/Model/Articles.php
+++ LoachBlog/Model/Articles.php
@@ -18,8 +18,10 @@ class Model_Articles extends PhalApi_Model_NotORM{
      */
     public function getAllArticles($offset,$num)
     {
-       $sql="select a.article_id as id,article_title as title,a.article_slug as tgas,a.article_created as unix_time,a.article_text as content,at.article_type_id as type_id,at.article_type_name as type_name from articles as a inner join relationships as rs on a.article_id=rs.article_id inner join article_type as at on at.article_type_id=rs.article_type_id and at.article_type_category='category' order by a.article_created desc limit 0,10";
-       return $this->getORM()->queryRows($sql);
+
+       $sql="select a.article_id as id,article_title as title,a.article_slug as tgas,a.article_created as unix_time,a.article_text as content,at.article_type_id as type_id,at.article_type_name as type_name from articles as a inner join relationships as rs on a.article_id=rs.article_id inner join article_type as at on at.article_type_id=rs.article_type_id and at.article_type_category='category' order by a.article_created desc limit :OFFSET,:NUM";
+       $params=array(':OFFSET'=>$offset,':NUM'=>$num);
+       return $this->getORM()->queryRows($sql,$params);
     }
     
     /**
